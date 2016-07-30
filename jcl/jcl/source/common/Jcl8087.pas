b501d126d5035e095342d758ddb4c2aa3373ee3f
LongDirtyAnimAlf <LongDirtyAnimAlf@gmail.com>
2016-07-30 10:55:18 +0200

    Small update to solve Delphi bug

--- jcl/jcl/source/common/Jcl8087.pas
+++ jcl/jcl/source/common/Jcl8087.pas
@@ -167,8 +167,10 @@ begin
   Set8087ControlWord((CW and $F3FF) or (Word(Rounding) shl 10));
 end;
 
+{$ifdef FPC}
 {$PUSH}
 {$R-}
+{$endif FPC}
 function Set8087ControlWord(const Control: Word): Word;
 var
   StackControl: Word;
@@ -178,7 +180,9 @@ asm
           FSTCW   Result         // save the old control word
           FLDCW   StackControl   // load the new control word
 end;
+{$ifdef FPC}
 {$POP}
+{$endif FPC}
 
 function ClearPending8087Exceptions: T8087Exceptions;
 
@@ -263,8 +267,10 @@ begin
     Include(Result, emPrecision);
 end;
 
+{$ifdef FPC}
 {$PUSH}
 {$R-}
+{$endif FPC}
 function SetMasked8087Exceptions(Exceptions: T8087Exceptions; ClearBefore: Boolean): T8087Exceptions;
 
   function ClearPendingExceptions: Word;
@@ -318,7 +324,9 @@ begin
   if (OldCW and $20) <> 0 then
     Include(Result, emPrecision);
 end;
+{$ifdef FPC}
 {$POP}
+{$endif FPC}
 
 function Mask8087Exceptions(Exceptions: T8087Exceptions): T8087Exceptions;
 begin
